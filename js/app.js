c5b84c313ef7265e7c6e7b8b0949dba186c4c941
ltraore2020 <lassana.traore2020@campus-eni.fr>
2021-03-15 10:09:04 +0100

    add download and contact form

--- js/app.js
+++ js/app.js
@@ -6,24 +6,24 @@ function submitForm(e) {
 
     let name = document.querySelector(".name").value;
     let email = document.querySelector(".email").value;
-    let projet = document.querySelector(".projet").value;
     let message = document.querySelector(".message").value;
+    let pass = "cqtgahvrrmtukacl";
 
-    sendEmail(name, email, projet, message);
+    sendEmail(name, email, message, pass);
     document.querySelector(".contact_form").reset();
 
 }
 
-function sendEmail(name, email, projet, message) {
+function sendEmail(name, email, message, pass) {
     Email.send({
         Host: "smtp.gmail.com",
         Username: 'lassana.traore2022@gmail.com',
-        Password: "cqtgahvrrmtukacl",
-        To: "lassana.traore2022@gmail.com",
+        Password: pass,
+        To: "lassana.traore2020@campus-eni.fr",
         From: `${email}`,
         Subject: `${name} sent you a message`,
         Body: `Name: ${name} <br/> 
                 Email: ${email} <br/>
                 Meessage: ${message}`,
-    }).then(message => alert("mail sent succesfully"));
+    }).then(message => alert("Mail envoyé avec succès"));
 }
